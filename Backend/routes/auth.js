82209614bea716640071a52496992dbf3dec0f63
Charan <charankulal0241@gmail.com>
2023-09-23 21:56:03 +0530

    Created Express validation for name email password fields

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,13 +1,27 @@
 const express=require('express')
 const User = require('../models/User')
 const router=express.Router()
+const {body, validationResult} = require('express-validator')
 
 //Create a User using POST
-router.post('/',(req,res)=>{
-    console.log(req.body)
-    var user=User(req.body)
-    user.save()
-    res.send(req.body)
+router.post('/',[
+    body('name','Enter a valid name').isLength({min:3}),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Enter a valid password').isLength({min:8}),
+],(req,res)=>{
     
+    const errors=validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    User.create({
+        name:req.body.name,
+        password:req.body.password,
+        email:req.body.email,
+    }).then(user=>res.json(user))
+    .catch(err=>{console.log(err)
+    res.json({error:'The Email Already Exists',message:err.message})
+    })
+       
 })
 module.exports=router
