e57c39dcd925fd9b094beffac97c50a051f25bde
Charan <charankulal0241@gmail.com>
2023-09-28 20:17:29 +0530

    Created a sample form for adding a note

--- Backend/index.js
+++ Backend/index.js
@@ -14,5 +14,5 @@ app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port,()=>
-    console.log(`Application is running in localhost:${port}`)
+    console.log(`iNotebook is running in localhost:${port}`)
 )
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,34 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center">This is iNotebook</h1>
+      <h2 className="text-center">Add a note</h2>
+      <form className="form" action="">
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+            Example textarea
+          </label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <div className="mb-3 text-center">
+    <button type="submit" className="btn btn-primary mb-3">Add Notes</button>
+  </div>
+      </form>
+      <h2 className="text-center">Your notes</h2>
     </div>
   );
 }
