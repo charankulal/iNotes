109f3839962ac56ed6c0f10ce4be33a40c04dc5a
Charan <charankulal0241@gmail.com>
2023-09-28 18:11:18 +0530

    Included Bootstrap and Done routing for the home about and navbar and created a navbar.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
 
 function App() {
   return (
     <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }
