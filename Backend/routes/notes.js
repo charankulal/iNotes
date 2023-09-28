7ce456cbfb8be4a63564eef981b9df4e3a5c14f5
Charan <charankulal0241@gmail.com>
2023-09-28 14:24:02 +0530

    Created updatenote endoint and added user validaton.

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -4,7 +4,7 @@ var fetchuser = require("../middleware/fetchuser");
 const { body, validationResult } = require("express-validator");
 const Notes = require("../models/Notes");
 
-// ROUTE 1:  Get all the notes using GET: "/api/auth/getuser" . Login required
+// ROUTE 1:  Get all the notes using GET: "/api/notes/getuser" . Login required
 
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   try {
@@ -16,7 +16,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   }
 });
 
-// ROUTE 2:  Add a new note using POST: "/api/auth/addnote" . Login required
+// ROUTE 2:  Add a new note using POST: "/api/notes/addnote" . Login required
 
 router.get(
   "/addnote",
@@ -29,25 +29,56 @@ router.get(
   ],
   async (req, res) => {
     try {
-        const { title, description, tag } = req.body;
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
+      const { title, description, tag } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
-    }
+      }
 
-    const note = new Notes({
+      const note = new Notes({
         title,
         description,
         tag,
         user: req.user.id,
-    });
-    const savedNote = await note.save();
-    console.log(savedNote);
-    res.json(savedNote);
+      });
+      const savedNote = await note.save();
+      console.log(savedNote);
+      res.json(savedNote);
     } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal server error");
+      console.error(error.message);
+      res.status(500).send("Internal server error");
     }
   }
 );
+
+// ROUTE 3:  Updating a existing note using POST: "/api/notes/updatenote" . Login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+    const {title,description,tag}=req.body
+
+    //Create new Notes object
+    const newNotes={}
+
+    if(title){
+        newNotes.title=title
+    }
+    if(description){
+        newNotes.description=description
+    }
+    if(tag){
+        newNotes.tag=tag
+    }
+    //Find the note to be updated 
+    var note=await Notes.findById(req.params.id)
+    if(!note)
+    {
+        res.status(404).send("Can't find any notes")
+    }
+    if(note.user.toString()!== req.user.id)
+    {
+      return res.status(401).send("Not Authorized")
+    }
+    note=await Notes.findByIdAndUpdate(req.params.id,{$set:newNotes},{new:true})
+    res.json({note})
+});
+
 module.exports = router;
